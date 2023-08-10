9f6fbff13625e7d589f3372248088d0f84d74ffa
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-10 22:42:40 +0300

    [Chore]: Add API

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ for HTTP authorization
 """
 
 from .auth import Auth
+import re
 
 
 class BasicAuth(Auth):
@@ -14,3 +15,35 @@ class BasicAuth(Auth):
     Basic Auth class
     for Basic HTTP authorization
     """
+    @staticmethod
+    def is_valid_basic_header(auth_header: str) -> str:
+        """
+        A simple function to validate headers
+        Args:
+            auth_header (str): string to validate against
+        Returns:
+            returns a boolean
+        Example
+            is_valid_header("Basic aXRzZm9zczppdHNmb3NzCg==")
+            # True
+            is_valid_header("Not a valid input")
+            # False
+        """
+        pattern = r'^Basic [a-zA-Z0-9\-]*'
+        return re.match(pattern, auth_header) is not None
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        extract the base64  from the header
+        Args:
+            authorization_header(str): the header in reference
+        Returns:
+            returns a string (base64)
+        """
+        if not authorization_header or not\
+                isinstance(authorization_header, str):
+            return None
+        if not BasicAuth.is_valid_basic_header(authorization_header):
+            return None
+        return authorization_header.split(" ")[1]
