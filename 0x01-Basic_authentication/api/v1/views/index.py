831c1ff3c79c51a20b681d78000e6db381a2c360
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-10 16:53:32 +0300

    [Chore]: Add API

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized_endpoint_requested():
+    """
+    handle unathorized requests to this
+    endpoint
+    """
+    abort(401)
