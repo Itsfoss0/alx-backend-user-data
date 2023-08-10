d5cb5e66f3b7af5fab6fba15a6146159fe6ee09b
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-10 21:56:32 +0300

    [Chore]: Updat the API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,6 +18,9 @@ auth = getenv('AUTH_TYPE')
 if auth:
     from api.v1.auth.auth import Auth
     auth = Auth()
+if auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Basic Auth
+this module provides the basic auth class
+for HTTP authorization
+"""
+
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic Auth class
+    for Basic HTTP authorization
+    """
