26de1247499de6246149b09d1e2ecc0a519e4bd2
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-10 17:14:57 +0300

    [Chore]: Add API

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -2,11 +2,10 @@
 """ DocDocDocDocDocDoc
 """
 
-from api.v1.views.index import *
-from api.v1.views.users import *
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-
+from api.v1.views.index import *   # noqa: E402
+from api.v1.views.users import *   # noqa: E402
 
 User.load_from_file()
