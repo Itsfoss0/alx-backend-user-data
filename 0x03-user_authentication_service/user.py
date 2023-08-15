070679f56aa51d2cb837c4cbc706d52f20685b5b
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-15 15:49:54 +0300

    [Chore]: Fix user model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -20,4 +20,4 @@ class User(Base):
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
-    reset_tokken = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
