bd3ce7c6640fdb1260469ee1756474551e3352b4
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-15 15:20:08 +0300

    [Chore]: Add User Model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+User model for ORM mapping
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    The user Model for mapping
+    Users in the ORM
+    """
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_tokken = Column(String(250), nullable=True)
