e947b105365e2ccecce048fd4524bcd28331679f
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-18 00:09:39 +0300

    [Chore]: Handle password reset

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -131,3 +131,18 @@ class Auth:
             "session_id": None
         }
         user = self._db.update_user(user_id, **new_session)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generate a password reset token for the user
+        Args:
+            email (str): user's email
+        Returns:
+            returns a string
+        """
+        user = self._db.find_user_by(email=email)
+        if user:
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        raise ValueError('User not found!')
