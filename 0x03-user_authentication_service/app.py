ff39cc5c98a1e225218c1f38bcfa1cb253b5500a
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-18 00:24:54 +0300

    [Chore]: Add password reset route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,21 @@ def profile():
     abort(403)
 
 
+@app.route("/password_reset", strict_slashes=False, methods=["POST"])
+def get_reset_password_token():
+    """
+    POST /password_reset
+    to reset user's password
+    """
+    email = request.form.get('email')
+    if email:
+        try:
+            r_token = AUTH.get_reset_password_token(email)
+            return jsonify({"email": f"{email}", "reset_token": f"{r_token}"})
+        except ValueError:
+            abort(403)
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
