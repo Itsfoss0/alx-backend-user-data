40b4e995aa2ff5d6a2c730fa4fafe0a3d1788c1a
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-17 23:13:25 +0300

    [Chore]: Add session auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,7 +4,8 @@
 Entry point for a simple flask application
 """
 
-from flask import (Flask, jsonify, request)
+from flask import (Flask, jsonify,
+                   request, make_response, abort)
 from auth import Auth
 
 
@@ -32,6 +33,28 @@ def users():
         return jsonify({"email": f"{email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", strict_slashes=False, methods=["POST"])
+def sessions():
+    """
+    Handle session flow
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            response = make_response(
+                {"email": f"{email}",
+                    "message": "logged in"}
+            )
+            response.set_cookie("session_id", session_id)
+            return response
+        abort(401)
+    except Exception:
+        abort(401)
 
 
 if __name__ == "__main__":
