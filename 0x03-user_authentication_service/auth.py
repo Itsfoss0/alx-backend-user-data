2da10046a5f6a475b6a2f8ee715df7110affb88a
Ebenezer <akhonyaebenezer@gmail.com>
2023-08-17 23:20:21 +0300

    [Chore]: Find User By session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -102,3 +102,19 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """
+        Get a user from the session ID
+        Args:
+            session_id (str): user's session ID
+        Returns:
+            returns a user (object of User)
+        """
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
